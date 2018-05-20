5359220c554b86ec8e817413bc87604e375436c5
zhangyinhao <zhangyinhao1234@163.com>
2018-05-20 19:36:58 +0800

    添加cname

--- hibernate-query-plugin/hibernate-in-springboot/src/main/java/com/binpo/web/service/BIExampleServiceImpl.java
+++ hibernate-query-plugin/hibernate-in-springboot/src/main/java/com/binpo/web/service/BIExampleServiceImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Service;
 import com.binpo.plugin.hibernate.IdEntity;
 import com.binpo.plugin.hibernate.query.page.v2.BaseServiceImpl;
 import com.binpo.pojo.Example;
-@Service
-public class BIExampleServiceImpl extends BaseServiceImpl<Example> implements IBIExampleService{
 
-	
+@Service
+public class BIExampleServiceImpl extends BaseServiceImpl<Example> implements IBIExampleService {
+
 }
