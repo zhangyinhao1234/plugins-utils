420ce7e8ef7e6d1bc11fd867d8a15a9cb63ce227
zhangyinhao <zhangyinhao1234@163.com>
2018-05-29 21:03:39 +0800

    添加单元测试代码

--- wechat-plugin/src/test/java/com/github/zhangyinhao1234/plugin/wechat_plugin/SendRedirectTest.java
+++ wechat-plugin/src/test/java/com/github/zhangyinhao1234/plugin/wechat_plugin/SendRedirectTest.java
@@ -3,32 +3,38 @@ package com.github.zhangyinhao1234.plugin.wechat_plugin;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Test;
+
 import com.alibaba.fastjson.JSON;
 import com.binpo.plugin.wechat.open.AccessToken;
 import com.binpo.plugin.wechat.open.WeChatOpen.Scope;
 import com.binpo.plugin.wechat.open.WeChatOpenWebUtil;
 
 public class SendRedirectTest {
+	@Test
 	public void testGetUrl() throws Exception {
-		String webPath = "http://dev.taocaimall.com/taocaimall";
+		
+		//获取从微信端打开的链接地址
+		//需要重定向的地址
+		String pagePath = "http://dev.taocaimall.com/taocaimall/superior/wechat/weChatShopIndex.htm";
 		//中间服务器，用来处理我们拼接好的参数，在这个中间服务器上会重定向到
 		String fullNotifyUrl = "http://u.taocai.mobi/taocaimall/wechat_redirec_center.json";
 		String state = "1234";// 随机数
-		String appId = "";
+		String appId = "wx2f5db1c382f9ca7d";
 		String sessionId = "adfsdkfsdkjnfjsdk";
 		Map<String, String> webParams = new HashMap<>();
 		String stateKey = WeChatOpenWebUtil.getstateKey(sessionId);
 		webParams.put("stateKey", stateKey);
 		String redirect_uri = WeChatOpenWebUtil
-				.createUniformRedirect_EncodeUri_2("",webPath + "/iphoneSEActivityIndex.htm", webParams);
+				.createUniformRedirect_EncodeUri_2(fullNotifyUrl,pagePath, webParams);
 		String authorizeUrl = WeChatOpenWebUtil.getAuthorizeUrl(redirect_uri, state, appId,Scope.snsapi_base);
 		System.out.println(authorizeUrl);// 微信重定向地址
 
-//		String code = "061cGRlN1lkTw51OFznN1C4XlN1cGRlJ";
-//		// appId = "";
-//		String appSecret = "";
-//		AccessToken accessToken = WeChatOpenWebUtil.getAccessToken(code, appId, appSecret);
-//		System.out.println(JSON.toJSONString(accessToken));
+		String code = "061cGRlN1lkTw51OFznN1C4XlN1cGRlJ";
+		String appSecret = "";
+		AccessToken accessToken = WeChatOpenWebUtil.getAccessToken(code, appId, appSecret);
+		System.out.println(JSON.toJSONString(accessToken));
 
+//		String url = WeChatOpenWebUtil.getSendRedirectInWeb(stateKey, code, webParams);
 	}
 }
